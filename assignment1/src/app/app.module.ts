ec663e84736b1ec2a1251a456d92c2f4eda6595b
Srujan <srujanp98@gmail.com>
2021-01-07 15:55:24 +0530

    Repo Added

---
+++ assignment1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import{SuccessAlertComponent} from './success-alert/success-alert.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
