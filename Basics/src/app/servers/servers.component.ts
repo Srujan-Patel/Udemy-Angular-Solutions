74953d7d3eb4f9ad39c2b1ae7534911af91d4265
Srujan <srujanp98@gmail.com>
2021-01-08 16:34:34 +0530

    'assignment3'

--- Basics/src/app/servers/servers.component.ts
+++ Basics/src/app/servers/servers.component.ts
@@ -11,22 +11,35 @@ export class ServersComponent implements OnInit {
   allowServer =false;
   serverName='TestServer';
   serverCreationStatus='Server not Created';
+  serverCreate= false;
+  servers=['TestServer1', 'TestServer2']
 
 constructor() {
 setTimeout(()=>{this.allowServer=true;}, 2000)
+this.status= Math.random() >=0 ?'online': 'offline';
    }
-  getstatus(){return this.status}
+ 
 
 
   ngOnInit(): void {
   }
 
 onCreateServer(){
+  this.serverCreate=true;
   this.serverCreationStatus= this.serverName + ' has been created ';
+  this.servers.push(this.serverName);
+
 }
 
 onUpdateServerName(event: any){
   this.serverName= event.target.value;
 }
+
+getColor(){
+
+  return this.status === 'online'? 'green': 'red';
+}
+
+
 
 }
