d731ed86ff953952a1859fb40a54a851d8d90340
Srujan <srujanp98@gmail.com>
2021-01-08 10:51:49 +0530

    'assignment2'

--- Basics/src/app/servers/servers.component.ts
+++ Basics/src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  serverId=10;
+  status='offline';
+  allowServer =false;
+  serverName='TestServer';
+  serverCreationStatus='Server not Created';
 
-  constructor() { }
+constructor() {
+setTimeout(()=>{this.allowServer=true;}, 2000)
+   }
+  getstatus(){return this.status}
+
 
   ngOnInit(): void {
   }
+
+onCreateServer(){
+  this.serverCreationStatus= this.serverName + ' has been created ';
+}
+
+onUpdateServerName(event: any){
+  this.serverName= event.target.value;
+}
 
 }
--- assignment2/src/app/app.module.ts
+++ assignment2/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import{ServerComponent} from './server/server.component';
-import { from } from 'rxjs';
-import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule,
