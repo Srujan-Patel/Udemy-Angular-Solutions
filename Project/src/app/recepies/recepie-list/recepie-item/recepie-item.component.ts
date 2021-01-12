602058dee0197e6da781eb905a33ae0903a8a4dc
Srujan <srujanp98@gmail.com>
2021-01-12 16:03:43 +0530

    'Project-Edited'

--- Project/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ Project/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Recepie} from '../../recepie.model';
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recepie-item.component.html',
   styleUrls: ['./recepie-item.component.css']
 })
 export class RecepieItemComponent implements OnInit {
-
+ @Input() recepie: Recepie;
   constructor() { }
 
   ngOnInit(): void {
--- Project/src/app/recepies/recepie-list/recepie-list.component.ts
+++ Project/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -10,10 +10,6 @@ export class RecepieListComponent implements OnInit {
 recepies: Recepie[]=[
   new Recepie('A test recepie', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?webp=true&quality=90&resize=440%2C400'),
   new Recepie('A test recepie', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?webp=true&quality=90&resize=440%2C400')
-
-
-
-
 ];
   constructor() { }
 
