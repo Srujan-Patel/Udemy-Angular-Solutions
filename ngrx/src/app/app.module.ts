44e1ff10478ecd9b475f1769adb27e6b461ab4f4
Srujan <srujanp98@gmail.com>
2021-02-04 14:48:11 +0530

    cli update

--- ngrx/src/app/app.module.ts
+++ ngrx/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
